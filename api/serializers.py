5cebcfdb660e7ee233f977c232bf0ad76c5b14d1
zouhair-ux <t8710727@gmail.com>
2026-02-25 14:28:44 +0000

    fix image url

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,33 @@
 from rest_framework import serializers
 from .models import Category, Product, Order
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name_fr', read_only=True)
-    
+
+    # 🔥 هذا اللي زدنا باش يرجّع رابط الصورة صحيح
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
 
+    # 🔥 هاد الدالة كتجيب URL ديال الصورة كامل
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
+
+
 class OrderSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name_fr', read_only=True)
-    
+
     class Meta:
         model = Order
         fields = '__all__'
